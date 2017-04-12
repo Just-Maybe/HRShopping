856aae08568d94e0cc4faf919904c79e6dc541d7
Dendi <122094709@qq.com>
2017-04-12 13:42:30 +0800

    Update ProductDescribeService.java

--- app/src/main/java/com/example/helloworld/huaruanshopping/api/ProductDescribeService.java
+++ app/src/main/java/com/example/helloworld/huaruanshopping/api/ProductDescribeService.java
@@ -23,16 +23,16 @@ import retrofit2.http.Query;
 
 public interface ProductDescribeService {
     //    /product_getProduct.action?pid=1
-//    http://119.29.24.119/api/products/details/1
+//    
     @GET("api/products/details/{id}")
     Observable<ProductBean> getProductDescribe(@Path("id") int id);
 
     //    获取评论列表
-//    http://www.chjcal.cc/api/comments/1?pageNum=1
+//    /api/comments/1?pageNum=1
     @GET("api/comments/{pid}")
     Observable<Comment> getComment(@Path("pid") int pid, @Query("pageNum") int pageNum);
 
-    //    http://localhost:8080/HR-Shop/api/comments/comment加参数
+    // HR-Shop/api/comments/comment加参数
     @Multipart
     @POST("api/comments/comment")
     Observable<Response> postComment(@Query("pid") int pid,
@@ -41,7 +41,7 @@ public interface ProductDescribeService {
                                      @Query("comment") String comment,
                                      @Part List<MultipartBody.Part> files);
 
-    //    http://localhost:8080/HR-Shop/api/comments/append_comment?comment_id=7&pid=1&uid=7&append_Comment=追加评论追加评论追加评论追加评论追加评论追加评论追加评论
+    //    /api/comments/append_comment?comment_id=7&pid=1&uid=7&append_Comment=追加评论追加评论追加评论追加评论追加评论追加评论追加评论
     @Multipart
     @POST("api/comments/append_comment")
     Observable<Response> postAppendComment(@Query("comment_id") int comment_id,
@@ -51,7 +51,7 @@ public interface ProductDescribeService {
                                            @Part List<MultipartBody.Part> files);
 
     //    评论上面的商品详情
-//    http://localhost:8080/HR-Shop/api/sorders/60?pid=1&uid=1
+// /api/sorders/60?pid=1&uid=1
     @GET("api/sorders/60")
     Observable<CommentProduct> getCommentProduct(@Query("pid") int pid, @Query("uid") int uid);
 }
